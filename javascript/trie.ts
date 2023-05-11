fdc20dd1f3ee27eb0daf59072395ad0e27fc3f62
Kristian Hansson <kristian.heikinpoika@gmail.com>
2023-05-12 00:13:52 +0300

    Refactors trie

--- javascript/trie.ts
+++ javascript/trie.ts
@@ -5,44 +5,64 @@
 
 import { isNil } from "./isNil.ts";
 
-export interface Trie<T> {
-  readonly [key: string]: Trie<T> | T;
+interface TrieNode<T> {
+  [key: string]: TrieNode<T> | T;
 }
 
+export class Trie<T> {
+  private root: TrieNode<T>;
 
-export function addToTrie<T>(trie: Trie<T>, word: string, value: T): Trie<T> {
-  if (word.length === 0) {
-    return { ...trie, 'value': value };
+  constructor() {
+    this.root = {};
   }
-  const key = word.charAt(0);
-  const child = trie[key] ?? {};
-  return { ...trie, [key]: addToTrie<T>(child as Trie<T>, word.slice(1), value) };
-}
 
-export function get<T>(trie: Trie<T>, word: string): T | null {
-  if (word.length === 0) {
-    return trie['value'] as T ?? null;
+  public add(word: string, value: T) {
+    let temp = this.root;
+    let rest = word;
+    while(rest.length > 0) {
+      const key = rest.charAt(0);
+      const child = temp[key] ?? {};
+      temp[key] = child;
+      temp = child;
+      rest = rest.slice(1);
+    }
+    temp['value'] = value;
   }
-  const key = word.charAt(0);
-  const child = trie[key];
-  if (!child) {
-    return null;
+
+  public get(word: string): T | null {
+    let pointer: TrieNode<T>= this.root;
+    let rest = word;
+    while (rest.length > 0) {
+      const key = rest.charAt(0);
+      const child = pointer[key] as TrieNode<T>;
+      if (isNil(child)) {
+        return null;
+      }
+      pointer = child;
+      rest = rest.slice(1);
+    }
+    return pointer['value'] as T ?? null;
   }
-  return get(child as Trie<T>, word.slice(1));
+
+  public contains(word: string): boolean {
+    return !isNil(this.get(word));
+  }
+
 }
+const start = new Date();
+const t = new Trie<string>();
+t.add('hello', 'greeting');
+t.add('hell', 'hell yeah');
+t.add('hel', 'Helsinki');
+t.add('app', 'sovellus');
+t.add('apple', 'omena');
 
-export function contains<T>(trie: Trie<T>, word: string): boolean {
-  return !isNil(get(trie, word));
-}
-
-const trie = addToTrie(addToTrie(addToTrie(addToTrie({}, 'apple', 'omena'), 'app', 'sovellus'), 'testing', 'kokeillaan'), 'spotify', 'musiikkia');
-
-console.log(JSON.stringify(trie, null, 2));
-console.log(get(trie, 'app'));
-console.log(get(trie, 'appley'));
-console.log(get(trie, 'apple'));
-console.log(get(trie, 'pask'));
-console.log(get(trie, 'piski'));
-console.log(get(trie, 'paska'));
-console.log(get(trie, 'spotify'));
-console.log(contains(trie, 'app'));
+console.log(t.get('hello'));
+console.log(t.get('hell'));
+console.log(t.get('hel'));
+console.log(t.get('he'));
+console.log(t.get('app'));
+console.log(t.get('apple'));
+const end = new Date();
+const diff = end.getTime() - start.getTime();
+console.log('took', diff, 'milliseconds');
