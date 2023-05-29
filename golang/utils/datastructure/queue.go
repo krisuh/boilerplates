793461cca6d5e0a836b859bf5d385666d9bc1c37
Kristian Hansson <kristian.heikinpoika@gmail.com>
2023-05-29 21:53:08 +0300

    Fixes queue

--- golang/utils/datastructure/queue.go
+++ golang/utils/datastructure/queue.go
@@ -1,6 +1,10 @@
 package datastructure
 
-import "golang.org/x/exp/slices"
+import (
+	"errors"
+
+	"golang.org/x/exp/slices"
+)
 
 type Queue[T any] struct {
 	Capacity int
@@ -12,16 +16,25 @@ func NewQueue[T any](capacity int) *Queue[T] {
 	return &Queue[T]{
 		Capacity: capacity,
 		Size:     0,
-		Items:    make([]T, capacity),
+		Items:    make([]T, 0),
 	}
 }
 
-func (q *Queue[T]) Enqueue(item T) {
-	q.Items = append(q.Items, item)
+func (q *Queue[T]) Enqueue(item T) error {
+	if q.Size < q.Capacity {
+		q.Items = append(q.Items, item)
+		q.Size = len(q.Items)
+		return nil
+	}
+	return errors.New("atempted to add to a full queue")
 }
 
-func (q *Queue[T]) Dequeue() *T {
-	item := q.Items[0]
-	q.Items = slices.Delete(q.Items, 0, 1)
-	return &item
+func (q *Queue[T]) Dequeue() (*T, error) {
+	if q.Size > 0 {
+		item := q.Items[0]
+		q.Items = slices.Delete(q.Items, 0, 1)
+		q.Size = len(q.Items)
+		return &item, nil
+	}
+	return nil, errors.New("attempted to dequeue an empty queue")
 }
--- golang/utils/datastructure/queue_test.go
+++ golang/utils/datastructure/queue_test.go
@@ -1,1 +1,17 @@
 package datastructure
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQueue(t *testing.T) {
+	q := NewQueue[int](10)
+	q.Enqueue(1)
+	q.Enqueue(2)
+	first, _ := q.Dequeue()
+	second, _ := q.Dequeue()
+	assert.Equal(t, 1, *first)
+	assert.Equal(t, 2, *second)
+}
